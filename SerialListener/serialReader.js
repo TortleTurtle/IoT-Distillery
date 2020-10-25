f8a922cb7fe0fe2b83951c2bee050030c6705228
TortleTurtle <danielmeijs@gmail.com>
2020-10-25 20:31:24 +0100

    update

--- SerialListener/serialReader.js
+++ SerialListener/serialReader.js
@@ -8,7 +8,7 @@ const url = "http://localhost:8000";
 
 // Read the port data
 parser.on('data', function (data) {
-    let string = data.toString();
+    let string = data.toString('ascii');
     let splitString = string.split("]");
     console.log(`current temperature: ${splitString[0]}`);
     postTemperature(url, splitString[0]);
